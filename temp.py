8441a958f357a6d8c6475485402cd18ba12da76c
paaaaaan <31851413+paaaaaan@users.noreply.github.com>
2017-09-22 21:06:46 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,25 @@
-print("######       ##   ###    ## ######### ##    ## #######  ####### ####     ##     ###     ##")
-print("##   ##     ###   ###    ##       ##  ##    ## ##   ##     ##    ##     ###     ####    ##")
-print("##    ##   ####   ####   ##      ##   ##    ## ##   ##     ##    ##    #  ##    ####    ##")
-print("##    ##  ##  ##  ####   ##     ##    ##    ## ##   ##     ##    ##    ##  ##   ## ##   ##")
-print("##   ##  ##   ##  ## ##  ##    ##     ##    ## ##   ##     ##    ##   ##    ##  ##  ##  ##")
-print("#####   ########  ##  ## ##   ##      ##    ## ##   ##     ##    ##  ########## ##   ## ##")
-print("##     ##     ##  ##   ####  ##       ##    ## ##   ## ##  ##    ##  ##      ## ##    ####")
-print("##    ##      ##  ##    ### ##        ##    ## ##   ## ##  ##    ##  ##      ## ##     ###")
-print("##   ##       ##  ##     ## ######### ######## #######  ####    #### ##      ## ##      ##")
+import pygame
+from pygame.locals import *
+import sys
+pygame.init()
+size = width, height = 1000, 1000
+speed = [1, 1]
+black = 249, 130, 57
+screen = pygame.display.set_mode(size)
+ball = pygame.image.load(r'C:\Users\潘\Desktop\pan.jpg')
+ballrect = ball.get_rect()
+while 1:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+  ballrect = ballrect.move(speed)
+  if ballrect.left < 0 or ballrect.right > width:
+    speed[0] = -speed[0]
+  if ballrect.top < 0 or ballrect.bottom > height:
+    speed[1] = - speed[1]
+  screen.fill(black)
+  screen.blit(ball, ballrect)
+  pygame.display.flip()
+
+  
+  参考源代码http://www.cnblogs.com/hongten/p/hongten_pygame_bouncing_ball.html
